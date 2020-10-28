bdd3273539db1ed61fd83475f78ea67ec2c581c2
DoctorSaiyubito <64788666+DoctorSaiyubito@users.noreply.github.com>
2020-10-28 15:25:32 +0100

    Add files via upload
    Adding cities' names and train stations.

--- generation_cartes_geopandas.py
+++ generation_cartes_geopandas.py
@@ -9,6 +9,8 @@ from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
 import matplotlib
 
+from shapely.geometry import Polygon
+
 def select_motifs(motifs,list_change,list_attribut,list_transport):
     # Create an empty list
     filter_arr = []
@@ -26,6 +28,29 @@ def select_motifs(motifs,list_change,list_attribut,list_transport):
 
 #theme = 'développement des trains'
 theme = 'développement des pistes cyclables'
+#area = 'France'
+area = 'Charente'
+
+if (area == 'France'):
+    #symbol size
+    motif_markersize = 0.2
+    gares_markersize = 0.4
+    linewidth = 0.5
+    #extent
+    xmin = -0.15e6
+    xmax = 1.5e6
+    ymin = 6e6
+    ymax = 7.25e6
+elif (area == 'Charente'):
+    #symbol size
+    motif_markersize = 50
+    gares_markersize = 100
+    linewidth = 3
+    #extent
+    xmin = 258219.2866
+    xmax = 572056.6832
+    ymin = 6432794.7987
+    ymax = 6615902.6867
 
 # LOAD DATA
 print("Loading Data...")
@@ -56,6 +81,9 @@ aires_urbaines.plot(column = "color",cmap="Blues",figsize=(40,20),edgecolor="gre
 if (theme == 'développement des trains'):
     railroad = geopandas.read_file("railroad_fr.geojson")
     
+    #gares
+    trainstations = geopandas.read_file("trainstations.geojson")
+    
     #motifs
     list_change = ['augmenter','construire','créer','développer','hausser']
     list_attribut = ['nombre','fréquence','quantité','rapidité','vitesse',None]
@@ -63,7 +91,10 @@ if (theme == 'développement des trains'):
     motifs = select_motifs(motifs,list_change,list_attribut,list_transport)
     
     print("- Add RailRoad...")
-    railroad.plot(ax=ax,color='red',linewidth=0.5)
+    railroad.plot(ax=ax,color='red',linewidth=linewidth)
+    
+    print("- Add Train stations...")
+    trainstations.plot(ax=ax,markersize=gares_markersize,color="red")
     
     label_lignes = 'voie ferrée'
 elif (theme == 'développement des pistes cyclables') :
@@ -76,20 +107,30 @@ elif (theme == 'développement des pistes cyclables') :
     motifs = select_motifs(motifs,list_change,list_attribut,list_transport)
     
     print("- Add BikeWay...")
-    bikeway.plot(ax=ax,color='red',linewidth=0.5)
+    bikeway.plot(ax=ax,color='red',linewidth=linewidth)
     
     label_lignes = 'piste cyclable'
 print("- Add Patterns...")
-motifs.plot(ax=ax,markersize=0.2,color="orange")
+motifs.plot(ax=ax,markersize=motif_markersize,color="orange")
+
+# LABEL
+communes = geopandas.read_file("communes_importantes.geojson")
+polygon = Polygon([(xmin, ymin), (xmin, ymax), (xmax, ymax), (xmax, ymin)])
+communes_clipped = geopandas.clip(communes, polygon)
+for idx, row in communes_clipped.iterrows():
+    ax.annotate(s=row['nom_commun'], xy=[row['geometry'].representative_point().x,row['geometry'].representative_point().y],horizontalalignment='center')
+
+print("- Add Cities' Names...")
 
 ax.axis("off")
-ax.set_xlim((-0.15e6,1.5e6))
-ax.set_ylim((6e6,7.25e6))
+
+ax.set_xlim((xmin,xmax))
+ax.set_ylim((ymin,ymax))
 
 
 print("- Add Scalebar and North Arrow...")
 # SCALE BAR AND NORTH ARROW
-scale_bar(ax, [0.6,0.05], 200,text_kwargs=dict(fontsize="22")) # changer fontsize pour la taille de la police
+scale_bar(ax, [0.1,0.05], 20,text_kwargs=dict(fontsize="22")) # changer fontsize pour la taille de la police
 
 x, y, arrow_length = 0.1, 0.7, 0.05
 ax.annotate('N', xy=(x, y), xytext=(x, y-arrow_length),
@@ -98,6 +139,7 @@ ax.annotate('N', xy=(x, y), xytext=(x, y-arrow_length),
             xycoords=ax.transAxes)
 
 print("- Add Legend...")
+
 # LEGEND
 cmap = matplotlib.cm.get_cmap('Blues')
 norm = matplotlib.colors.Normalize(vmin=0, vmax=3)
@@ -116,11 +158,16 @@ legend_elements = [Line2D([0], [0], color='r', lw=4, label=label_lignes),
                    Patch(facecolor=cmap(norm(3)), edgecolor='gray',
                          label='Pôle')]
 
-legend = ax.legend(handles=legend_elements, loc='right',title="Légende",fontsize="16") # changer fontsize pour la taille de la police
-legend.get_title().set_fontsize('18') # Augmenter ou diminuer valeur pour la taille de la police du titre de la légende
+if (theme == 'développement des trains'):
+    legend_elements.insert(1,Line2D([0], [0], marker='o', color='w', label='Gares', markerfacecolor='red', markersize=15))
+
+legend = ax.legend(handles=legend_elements, loc='best',title="Légende",fontsize="16") # changer fontsize pour la taille de la police
+legend.get_title().set_fontsize('18') # Augmenter ou diminuer valeur pour la taille de la police du titre de la légende
 
 print("DONE !")
-# SAVE FIGURE TO PDF
+# SAVE FIGURE TO PNG
 fig = matplotlib.pyplot.gcf()
 fig.set_size_inches(40, 20)
-fig.savefig("carte.pdf",bbox_inches="tight")
+fig.savefig('carte1bis.png', dpi=300,bbox_inches="tight")
+# SAVE FIGURE TO PDF
+#fig.savefig('carte2bis.pdf',bbox_inches='tight')
